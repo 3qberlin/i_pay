41b935712634a742f72a818ab09e8d5e31a770dc
Berlin <3q.berlin@gmail.com>
2023-08-25 18:59:53 +0800

    justify hire

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base:process.env.NODE_ENV === 'production' ? '/3qberlin.github.io/' : '/',
+  base:process.env.NODE_ENV === 'production' ? '/i_pay/' : '/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
